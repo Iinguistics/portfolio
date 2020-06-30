c0324e8642c62543ede924b88eaa5a7d28254ff7
James Goytia <jmsgoytia@gmail.com>
2020-06-29 23:07:13 -0700

    added img & text for homeMobile header

--- src/components/homeMobile.vue
+++ src/components/homeMobile.vue
@@ -1,7 +1,13 @@
 <template>
 <div>
- 
-
+ <div class="header-img">
+  <img src="../assets/dev-img-mobile.jpg"/>
+  <div class="header-details">
+    <h5>I build amazing experiences</h5>
+   <p>Front End Dev from El Dorado Hills, Ca</p>
+  </div>
+ </div>
+sddfsdf
 </div>
  
 
@@ -44,6 +50,39 @@ export default {
 
 
 
+@media(max-width: 768px) {
+
+   .header-img img{
+   width: 100%;
+  height: 350px;
+  border-radius: 5px;
+
+}
+
+.header-details{
+  position: absolute;
+  top: 30%;
+  left: 5%;
+}
+.header-details-gone{
+  display:none;
+}
+
+.header-details h5{
+  color: #fff;
+  font-weight: 500;
+  letter-spacing: 0.15em;
+  margin-bottom: 0.5em;
+  text-transform: uppercase;
+  font-size: 28px;
+}
+.header-details p{
+  color: #fff ;
+  font-size: 18px ;
+}
+   
+
+}
 
 
 
